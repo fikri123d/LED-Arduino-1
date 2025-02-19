c15d69f1b0c1a21e9cbc7e7fbb8c766f5ad3450d
Fikri Aly Amades <fikrialyamades@gmail.com>
2025-02-19 14:10:42 +0700

    Update main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int ledm = 8;
+int ledk = 9;
+int ledh = 10;
+
+
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Serial.begin(9600);
+pinMode(ledm, OUTPUT);
+pinMode(ledk, OUTPUT);
+pinMode(ledh, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+digitalWrite(ledm, HIGH);
+digitalWrite(ledk, LOW);
+digitalWrite(ledh, LOW);
+Serial.println("merah menyala");
+Serial.println("kuning mati");
+Serial.println("hijau mati");
+delay(3000);
+digitalWrite(ledm, LOW);
+digitalWrite(ledk, HIGH);
+digitalWrite(ledh, LOW);
+Serial.println("merah mati");
+Serial.println("kunig menyala");
+Serial.println("hijau mati");
+delay(1000);
+digitalWrite(ledm, LOW);
+digitalWrite(ledk, LOW);
+digitalWrite(ledh, HIGH);
+Serial.println("merah mati");
+Serial.println("kuning mati");
+Serial.println("hijau menyala");
+delay(2000);
 }
